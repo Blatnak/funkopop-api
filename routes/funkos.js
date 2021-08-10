748eb96771a680fc5e5e723e8a97cb601c45fcc6
Luis Fernando <luisfernando_181818@hotmail.com>
2021-08-09 21:16:57 -0500

    adding create-funko route

--- routes/funkos.js
+++ routes/funkos.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { getFunkos, createFunko } = require('../controllers/funkos');
 
-router.post('/', createFunko);
+router.post('/create-funko', createFunko);
 router.get('/', getFunkos);
 
 module.exports = router;
